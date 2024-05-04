29b4ae7d4da64d2f6f6683dad21b324c0e86e61f
Abhishek Agrawal <abhishek.ag64064@gmail.com>
2024-05-04 10:34:39 +0530

    removing unused databases

--- database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("sequalize-video", "root", "$udhiR1ag", {
-  dialect: "mysql",
-  port: 3306,
-  host: "localhost",
-});
-module.exports = sequelize;
